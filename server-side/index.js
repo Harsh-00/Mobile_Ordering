638d71f57a175f54e9d02847a310ae264c48952f
Harsh Agarwal <harshulkh@gmail.com>
2023-12-26 04:50:03 +0530

    url update

--- server-side/index.js
+++ server-side/index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 app.use(express.json());
 app.use(
 	cors({
-		origin: ["http://localhost:3000"],
+		origin: ["https://mobile-ordering-client.vercel.app"],
 		methods: ["GET", "POST"],
 		credentials: true,
 	})
