128cc414bfbbf623504d871a63cf550b0502ab33
Harsh Agarwal <harshulkh@gmail.com>
2023-12-26 05:25:22 +0530

    up

--- server-side/index.js
+++ server-side/index.js
@@ -3,15 +3,17 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 
-app.use(express.json());
-app.use(
-	cors({
-		origin: ["https://mobile-ordering-client.vercel.app"],
-		methods: ["GET", "POST"],
-		credentials: true,
-	})
-);
+app.use(function (request, response, next) {
+	response.header("Access-Control-Allow-Origin", "*");
+	response.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
+app.use(cors());
 
+app.use(express.json());
 app.use("/", (req, res) => {
 	return res.send("Hello World");
 });
